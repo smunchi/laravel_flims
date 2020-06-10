fb14b340654e7a8f0f3309eb1754b482018ea444
Md. Salahuddin <salah_cse_mbstu@yahoo.com>
2020-06-10 22:31:29 +0600

    after login user will be redirected to /flims route, flims controller and view file added

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/flims';
 
     /**
      * Create a new controller instance.
--- resources/views/flims/index.blade.php
+++ resources/views/flims/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Home</div>
+                <div class="card-header">Flims</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    Welcome to Flims App
+                    List of flims
                 </div>
             </div>
         </div>
